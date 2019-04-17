e946cb937232061db4ed197395ad77c7cfcace3d
Elliot Park <elliotpark@Elliots-MacBook-Pro.local>
2019-04-17 14:58:39 -0700

    asidfnae

---
+++ src/components/Scatter.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+`;
+
+
+export default class Area extends React.Component {
+  constructor (props) {
+    super(props)
+
+    // Feed the highchart chart options here
+    this.options = {
+      chart: {
+        align: 'center',
+        backgroundColor: null,
+        borderWidth: 0,
+        type: 'column',
+        margin: [2, 0, 2, 0],
+        width: 500,
+        height: 1000,
+        style: {
+          overflow: 'visible',
+        },
+        skipClone: true
+      },
+      title: {
+        text: ''
+      },
+      credits: {
+        enabled: false
+      },
+      xAxis: {
+        labels: {
+          enabled: false
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickPositions: []
+      },
+      yAxis: {
+        endOnTick: false,
+        startOnTick: false,
+        labels: {
+          enabled: false
+        },
+        title: {
+          text: null
+        },
+        tickPositions: [0]
+      },
+      legend: {
+        enabled: false
+      },
+      tooltip: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          animation: false,
+          lineWidth: 1,
+          shadow: false,
+          states: {
+            hover: {
+              enabled: false
+            }
+          },
+          marker: {
+            radius: 1,
+            states: {
+              hover: {
+                radius: 2
+              }
+            }
+          },
+          fillOpacity: 0.25
+        },
+        column: {
+          negativeColor: '#910000',
+          borderColor: 'silver'
+        }
+      },
+      series: [{
+
+        // Data is just an array that you can assign values in the render function
+        data: null,
+      }],
+       loading: false
+      }
+  }
+
+  // Render Function
+  render() {
+
+    // Assign options variable to the Sparkline object's options
+    const options = { ...this.options }
+
+    options.series[0].data = this.props.data
+
+    // assign the color to be the user inputted color; otherwise do nothing and use the default
+    options.plotOptions.series.color = this.props.config.color ? this.props.config.color[0] : null
+
+    // Use the width and height that the user gives, or use default
+    options.chart.width = this.props.config.chart_width
+    options.chart.height = this.props.config.chart_height
+
+    return (
+      <Container>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        />
+      </Container>
+    )
+  }
+}
---
+++ src/scatter-chart.js
@@ -0,0 +1,76 @@
+import SomeClass from './components/InBetweenClass'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+looker.plugins.visualizations.add({
+  options: {
+    color: {
+      type: "array",
+      label: "Color",
+      display: "color",
+      default: "#5b5d9a"
+    },
+   chart_width: {
+      type: "string",
+      label: "Chart Width",
+      default: 100,
+    },
+    chart_height: {
+      type: "string",
+      label: "Chart Height",
+      default: 100,
+    }
+  },
+
+  create: function(element, config) {
+    element.innerHTML = `
+      <style>
+        .scatter-chart {
+          /* Vertical centering */
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+        }
+        .highcharts-container {
+          margin: 0 auto;
+        } 
+        
+      </style>
+    `;
+
+    let container = element.appendChild(document.createElement("div"));
+    container.className = "scatter-chart";
+
+    this._textElement = container.appendChild(document.createElement("div"));
+
+    this.chart = ReactDOM.render(
+      <InBetweenClass
+        done={false}
+      />,
+      this._textElement
+    );
+
+  },
+
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    this.clearErrors();
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
+      return;
+    }
+
+    this.InBetweenClass = ReactDOM.render(
+      <SomeClass
+        config={config}
+        data={data}
+        done={done}
+        queryResponse={queryResponse}
+      />,
+      this._textElement
+    );
+
+    done()
+  }
+});
