0840fd237f4116a0af7bd38ce482091c88bc2bf0
Elliot Park <elliotpark@Elliots-MacBook-Pro.local>
2019-04-18 13:41:47 -0700

    Updating dist file

--- src/components/InBetweenClass.js
+++ src/components/InBetweenClass.js
@@ -23,12 +23,12 @@ export default class Hello extends React.Component {
     
     // Split the data so we can end up in this format for our dates: Date.UTC(2015, 11, 01)
     let predataToRender = this.props.data.map(d => {
-      return [(d[this.props.queryResponse.fields.dimensions[0].name].value).split("-"),d[this.props.queryResponse.fields.dimensions[1].name].value, d[this.props.queryResponse.fields.dimensions[2].name].value]
+      return [(d[this.props.queryResponse.fields.dimensions[0].name].value).split("-"),d[this.props.queryResponse.fields.dimensions[1].name].value, d[this.props.queryResponse.fields.dimensions[2].name].value, d[this.props.queryResponse.fields.dimensions[3].name].value]
     })
 
     // Want to end up with an array that is filled with [x,y] arrays, i.e. multiple two value arrays
     let dataToRenderAsArray = predataToRender.map(d => {
-      return [Date.UTC(parseInt(d[0][0]),parseInt(d[0][1])-1,parseInt(d[0][2])), d[1], d[2]]
+      return [Date.UTC(parseInt(d[0][0]),parseInt(d[0][1])-1,parseInt(d[0][2])), d[1], d[2], d[3]]
     })
 
     // Now we want to end up with an array of JSON blobs rather than an array of arrays like we have right now
@@ -43,13 +43,15 @@ export default class Hello extends React.Component {
       some_json_blob.y = element[1]
       some_json_blob.name = element[2]
 
-      let probability = element[3]
-      if(probability > 50) {
-          some_json_blob.color = "#0000ff"
-      } 
-      else {
-        some_json_blob.color = "#ff0000"
-      }
+      some_json_blob.probability = element[3]
+
+      // let probability = element[3]
+      // if(probability > 50) {
+      //     some_json_blob.color = "#0000ff"
+      // } 
+      // else {
+      //   some_json_blob.color = "#ff0000"
+      // }
 
 
       // plop the filled array into dataToRender
