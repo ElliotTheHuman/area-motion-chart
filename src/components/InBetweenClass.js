7edf63a2bfd74569723d3c86f07340c3e71ff5a6
Elliot Park <elliotpark@Elliots-MacBook-Pro.local>
2019-04-19 11:44:35 -0700

    Adding console.log to figure out waht this.props.data looks like

--- src/components/InBetweenClass.js
+++ src/components/InBetweenClass.js
@@ -21,18 +21,45 @@ export default class Hello extends React.Component {
 
   render() {
     
-    // this.props.data is an array of json objects. In order to pull out data (which is in the form of JSON) we need to dive into the queryResponse 
-    let predataToRender = this.props.data.map(d => 
-    {
-      return [(d[this.props.queryResponse.fields.dimensions[0].name].value).split("-"),d[this.props.queryResponse.fields.dimensions[1].name].value, d[this.props.queryResponse.fields.dimensions[2].name].value, d[this.props.queryResponse.fields.dimensions[3].name].value, d[this.props.queryResponse.fields.dimensions[4].name].value]
-    })
+    let numberOfRows = this.props.data.length
+    let numberOfDimensions = this.props.queryResponse.fields.dimensions.length
+    let dataToRenderAsArray = []
+    let dataRaw = this.props.data // array of data, each element is a JSON object representing a row
+    
+    // First loop iterates through every row of data
+    for(let i = numberOfRows - 1; i >= 0; i--) {
+      // We're going to pump temp_array into our dataToRenderAsArray array
+      let temp_array = []
 
-    console.log(this.props.queryResponse)
+      // Second loop iterates through each column, grabbing the values of the dimensions for a given row
+      for(let j = 0; j < numberOfDimensions; j++) {
 
-    // Want to end up with an array that is filled with [x,y] arrays, i.e. multiple two value arrays
-    let dataToRenderAsArray = predataToRender.map(d => {
-      return [Date.UTC(parseInt(d[0][0]),parseInt(d[0][1])-1,parseInt(d[0][2])), d[1], d[2], d[3], d[4]]
-    })
+        // If it's the first dimension, then we need to convert our date string into an epoch numerical value
+        if(j = 0) {
+          let dateAsArray = dataRaw[this.props.queryResponse.fields.dimensions[j].name].value.split("-")
+          let year = parseInt(date[0])
+          let month = parseInt(date[1])
+          let day = parseInt(date[2])
+          let dateAsEpoch = Date.UTC(year, month-1, day)
+
+          temp_array.push(dateAsEpoch)
+        }
+        // Otherwise we just push the dimenison value in
+        else {
+          temp_array.push(dataRaw[this.props.queryResponse.fields.dimensions[j].name].value)
+        }
+      }
+
+      dataToRenderAsArray.push(temp_array)
+    }
+      /* 
+        By the end we want an array that has these things:
+        (1) date in epoch - X-axis
+        (2) days open - Y-axis
+        (3) opportunity name - Tooltip Title
+        (4) probability - Marker Color
+        (5) amount, aka ACV - Marker size
+      */
 
     // Now we want to end up with an array of JSON blobs rather than an array of arrays like we have right now
     // Start with an empty array, and we'll push in JSON blobs that are equivalent to the arrays in dataToRenderAsArray
