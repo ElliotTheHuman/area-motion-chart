35a111ea3d3266c72bb8cfdd17964edf2069bd1b
Elliot Park <elliotpark@Elliots-MacBook-Pro.local>
2019-04-19 13:48:12 -0700

    Commit to it.

--- src/components/InBetweenClass.js
+++ src/components/InBetweenClass.js
@@ -21,7 +21,7 @@ export default class Hello extends React.Component {
 
   render() {
 
-
+/*
     // Refactored creation of dataToRenderAsArray
   
     let numberOfRows = this.props.data.length
@@ -30,23 +30,8 @@ export default class Hello extends React.Component {
     let dataRaw = this.props.data // array of data, each element is a JSON object representing a row
     
     // First loop iterates through every row of data
-    // for(let i = numberOfRows - 1; i >= 0; i--) {
-    //   // We're going to pump temp_array into our dataToRenderAsArray array
-    //   let temp_array = []
-
-      
-    //   //  By the end we want an array that has these things:
-    //   //  (1) date in epoch - X-axis
-    //   //  (2) days open - Y-axis
-    //   //  (3) opportunity name - Tooltip Title
-    //   //  (4) probability - Marker Color
-    //   //  (5) amount, aka ACV - Marker size
-
-    //   dataToRenderAsArray.push(temp_array)
-    // }
-
-    dataRaw.forEach(function(element) {
-
+    for(let i = numberOfRows - 1; i >= 0; i--) {
+      // We're going to pump temp_array into our dataToRenderAsArray array
       let temp_array = []
 
       // Second loop iterates through each column, grabbing the values of the dimensions for a given row
@@ -55,29 +40,38 @@ export default class Hello extends React.Component {
         // If it's the first dimension, then we need to convert our date string into an epoch numerical value
         if(j = 0) {
 
-          let dateAsArray = (element[this.props.queryResponse.fields.dimensions[j].name].value).split("-")
+          let dateAsArray = (dataRaw[i][this.props.queryResponse.fields.dimensions[j].name].value).split("-")
           let dateAsEpoch = Date.UTC(parseInt(dateAsArray[0]), parseInt(dateAsArray[1])-1, parseInt(dateAsArray[2]))
 
           temp_array.push(dateAsEpoch)
         }
         // Otherwise we just push the dimenison value in
         else {
-          temp_array.push(element[this.props.queryResponse.fields.dimensions[j].name].value)
+          temp_array.push(dataRaw[i][this.props.queryResponse.fields.dimensions[j].name].value)
         }
-
-        dataToRenderAsArray.push(temp_array)
       }
+
+      
+      //  By the end we want an array that has these things:
+      //  (1) date in epoch - X-axis
+      //  (2) days open - Y-axis
+      //  (3) opportunity name - Tooltip Title
+      //  (4) probability - Marker Color
+      //  (5) amount, aka ACV - Marker size
+
+      dataToRenderAsArray.push(temp_array)
+    }
+*/
+
+    let predataToRender = this.props.data.map(d => 
+    {
+      return [(d[this.props.queryResponse.fields.dimensions[0].name].value).split("-"),d[this.props.queryResponse.fields.dimensions[1].name].value, d[this.props.queryResponse.fields.dimensions[2].name].value, d[this.props.queryResponse.fields.dimensions[3].name].value, d[this.props.queryResponse.fields.dimensions[4].name].value]
     })
 
-    // let predataToRender = this.props.data.map(d => 
-    // {
-    //   return [(d[this.props.queryResponse.fields.dimensions[0].name].value).split("-"),d[this.props.queryResponse.fields.dimensions[1].name].value, d[this.props.queryResponse.fields.dimensions[2].name].value, d[this.props.queryResponse.fields.dimensions[3].name].value, d[this.props.queryResponse.fields.dimensions[4].name].value]
-    // })
-
-    // // Want to end up with an array that is filled with [x,y] arrays, i.e. multiple two value arrays
-    // let dataToRenderAsArray = predataToRender.map(d => {
-    //   return [Date.UTC(parseInt(d[0][0]),parseInt(d[0][1])-1,parseInt(d[0][2])), d[1], d[2], d[3], d[4]]
-    // })
+    // Want to end up with an array that is filled with [x,y] arrays, i.e. multiple two value arrays
+    let dataToRenderAsArray = predataToRender.map(d => {
+      return [Date.UTC(parseInt(d[0][0]),parseInt(d[0][1])-1,parseInt(d[0][2])), d[1], d[2], d[3], d[4]]
+    })
 
     // Now we want to end up with an array of JSON blobs rather than an array of arrays like we have right now
     // Start with an empty array, and we'll push in JSON blobs that are equivalent to the arrays in dataToRenderAsArray
