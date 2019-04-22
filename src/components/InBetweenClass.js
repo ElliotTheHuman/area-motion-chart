cb082e870535dcac86cf40d3b371e8419c0920cb
Elliot Park <elliotpark@Elliots-MacBook-Pro.local>
2019-04-21 20:29:38 -0700

    Removing intermediary step of converting query result JSON blobs into arrays

--- src/components/InBetweenClass.js
+++ src/components/InBetweenClass.js
@@ -21,17 +21,16 @@ export default class Hello extends React.Component {
 
   render() {
 
-    // Attempt at a refactor. Performance-wise, this thing kills the browser.
+    // Take a JSON blob from the query results, then convert it into a JSON blob High charts can display
+    let dataToRender = []
+    
+    for (let x = dataToRender.length - 1; x >= 0; x--) {
+      let temp_json_blob = {}
+      let number_of_dimensions = this.props.queryResponse.fields.dimensions.length
 
-    let dataToRenderAsArray = this.props.data.map(d => 
-    {
+      for(let i = 0; i < number_of_dimensions; i++) {
 
-      let temp_array = []
-      let numberOfDimensions = this.props.queryResponse.fields.dimensions.length
-
-      for(let i = 0; i < numberOfDimensions; i++) {
-        console.log(numberOfDimensions)
-
+        // X Axis: Close Date Dimension
         if(i == 0) {
 
           let dateAsArray = d[this.props.queryResponse.fields.dimensions[i].name].value.split("-") // splits a date string into a three-piece array
@@ -39,67 +38,38 @@ export default class Hello extends React.Component {
           let month = parseInt(dateAsArray[1])
           let day = parseInt(dateAsArray[2])
 
-          temp_array.push(Date.UTC(year,month,day))
+          temp_json_blob.x = (Date.UTC(year,month,day))
         }
-        else {
+        // Y Axis: Days Open Dimension
+        else if(i == 1) {
+          temp_json_blob.y = d[this.props.queryResponse.fields.dimensions[i].name].value
+        }
+        // Tooltip Header: Opportunity Name
+        else if(i == 2) {
+          temp_json_blob.name = d[this.props.queryResponse.fields.dimensions[i].name].value
+        }
+        // Marker Color: Probability
+        else if(i == 3) {
+          let probability = d[this.props.queryResponse.fields.dimensions[i].name].value
 
-          temp_array.push(d[this.props.queryResponse.fields.dimensions[i].name].value)
+            // Color Assignment
+            if (probability > 50) {
+              temp_json_blob.color = "#0000FF"
+            } 
+            else {
+              temp_json_blob.color = "#FF0000"
+            }
+        }
+        // Marker Radius: Deal Size
+        else if(i == 4) {
+          // Some jank scaling, might want to use log to get the right proportions?
+          temp_json_blob.markder.radius = d[this.props.queryResponse.fields.dimensions[i].name].value/100000*5
         }
       }
 
-      return temp_array
-    })
-
-  
-
-
-    // // For every element in the "data" array, 
-    // let predataToRender = this.props.data.map(d => 
-    // {
-    //   return [(d[this.props.queryResponse.fields.dimensions[0].name].value).split("-"),d[this.props.queryResponse.fields.dimensions[1].name].value, d[this.props.queryResponse.fields.dimensions[2].name].value, d[this.props.queryResponse.fields.dimensions[3].name].value, d[this.props.queryResponse.fields.dimensions[4].name].value]
-    // })
-
-
-    // // Want to end up with an array that is filled with [x,y] arrays, i.e. multiple two value arrays
-    // let dataToRenderAsArray = predataToRender.map(d => {
-    //   return [Date.UTC(parseInt(d[0][0]),parseInt(d[0][1])-1,parseInt(d[0][2])), d[1], d[2], d[3], d[4]]
-    // })
-
-    // Now we want to end up with an array of JSON blobs rather than an array of arrays like we have right now
-    // Start with an empty array, and we'll push in JSON blobs that are equivalent to the arrays in dataToRenderAsArray
-    let dataToRender =[]
-
-    dataToRenderAsArray.forEach(function(element) {
-      // Start with an empty blob
-      let some_json_blob = {}
-
-      some_json_blob.x = element[0]
-      some_json_blob.y = element[1]
-      some_json_blob.name = element[2]
-
-      // For now, if our prob is > 50, make the dot green. Otherwise, make it red.
-      let probability = element[3]
-
-      if (probability > 50) {
-        some_json_blob.color = "#0000FF"
-      } else {
-        some_json_blob.color = "#FF0000"
-      }
-
-      // Use log to scale scatter dot size
-      some_json_blob.marker = {}
-      some_json_blob.marker.radius = (element[4])/100000*5
-
-      /////////// Potential Colorizing function ///////////
-      // https://stackoverflow.com/questions/16360533/calculate-color-hex-having-2-colors-and-percent-position
-      /////////////////////////////////////////////////////
-
-      // plop the filled array into dataToRender
-      dataToRender.push(some_json_blob)
-    }) 
-
-    // Elliot Note: Needed to reverse the dataToRender array since the data was backwards
-    dataToRender = dataToRender.reverse()
+      // temp_json_blob should be ready to go for Highcharts now
+      dataToRender.push(temp_json_blob)
+    }
 
     // So we create a Sparkline component with these specifications
     const scatter_chart = (
